43a098518ccca26df3c95b50cb3889e3c14c16ec
PFLeget <leget@stanford.edu>
2020-12-27 16:53:21 +0100

    add print for the test

--- treegp/__init__.py
+++ treegp/__init__.py
@@ -4,6 +4,7 @@ treegp.
 
 # The version is stored in _version.py as recommended here:
 # http://stackoverflow.com/questions/458550/standard-way-to-embed-version-into-python-package
+print('TEST CONSTANT')
 from ._version import __version__, __version_info__
 
 from .gp_interp import GPInterpolation
